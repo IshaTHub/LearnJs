bc92e537a16b8e88df175546b8d4ac7726a0cec5
Isha Tiwari <85096427+IshaT-Hub@users.noreply.github.com>
2024-06-25 07:33:48 +0000

    coversion-typeof

---
+++ 01-basic/01-dataConversion.js
@@ -0,0 +1,11 @@
+// let score = "33abc"
+// console.log(typeof(score));
+
+// let changedScore = Number(score)
+// console.log(typeof changedScore);
+// console.log(changedScore);
+
+let issScore = "";
+let changedScore = Boolean(issScore)
+
+console.log( changedScore );
