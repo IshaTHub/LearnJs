c5650f566c80aba93e463e8f4141ee7add60c7f1
Isha Tiwari <85096427+IshaT-Hub@users.noreply.github.com>
2024-07-08 10:59:43 +0000

    scopes

---
+++ 03-basic/scopes.js
@@ -0,0 +1,19 @@
+let a = 200
+if(true){                     // Block Scope
+    let a = 10
+    //console.log("Inner: ", a);
+}
+
+//console.log("Outer: ", a);   // Global Scope
+
+//++++++++++INTRESTING++++++++++++
+
+function addOne(num) {
+    return num+1
+}
+addOne(5)    // if called before initialization no error will occur.
+
+const addTwo = function(num1) {
+    return num1 + 2
+}
+addTwo(5)   // if called before initialization Reference error will occur.
