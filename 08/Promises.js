c5bf83767e0d007f0f7651a2f6f1ffdf81e73f12
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-04-10 23:07:26 +0530

    Create Promises.js

---
+++ 08/Promises.js
@@ -0,0 +1,43 @@
+//PROMISE
+
+//promises are used to handle async operations in javascript.
+// Promise is an object representing eventual completion or failure of an async operation.
+
+const cart = ['shoes', 'bag', 'hats'];
+
+// here we are "PASSING" callback to another function
+createOrder(cart,function(){
+proceedToPayment(orderId);
+}); 
+
+//the above leads to inversion of control problem
+
+const promise = createOrder(cart);    // this line will return us a promise i.e an empty object 
+//{data:undefined}
+// later this object will be filled with data automatically after async time it will take {data: orderDetails}
+
+//we will be "ATTACHING" this callback func to our promise object. Which will be called
+promise.then(function(){
+proceedToPayment(orderId);
+});
+
+//promise gives us the gurantee that it will call this callback funtion whenever there is data inside the promise object
+// it will call this just ONCE
+
+const ANY_API = "https://api.github.com/users/ishathub"
+
+const usre = fetch(ANY_API);  // we will get promise object inside user
+//api given by browser to us to make external calls
+// this fetch function returns us a promise
+
+// [PromiseState] : initally it will be in "pending" state  "fullfilled" & "rejected"
+// [PromiseResult] : data that fetch method will return
+
+//PromiseStates are immutable
+
+//to attach callback
+user.then(fucntion(data){
+  console.log(data);
+})
+
+
