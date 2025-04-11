caeec46fe2e9bb47b99f4fed4177b5c0f4b9d67f
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-04-11 18:06:23 +0530

    Create asyncAwait.js

---
+++ 08/asyncAwait.js
@@ -0,0 +1,26 @@
+// Async
+// it is a keyword used before a function to create a async function
+
+const p = new Promise((resolve, reject) => {
+  resolve("Promise resolved value!!")
+})
+
+// this async function ALWAYS retrurns a PROMISE
+async function getData(){
+  return p;     
+}
+//if it returns a value then it will be automatically wrapped inside a promise and return a promise
+
+
+
+const dataPromise = getData();
+
+dataPromise.then(res => console.log("res"));
+
+
+//using async with await
+//async and await combo are used to handle promises
+
+//how do we used to handle promises before async and await
+function getData(){
+  p.then(res =>console.log(res));
