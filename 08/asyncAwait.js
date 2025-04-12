9141a1b0c50b3bfe0bc956087cd03f32dc7bd78d
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-04-12 23:46:05 +0530

    Update asyncAwait.js

--- 08/asyncAwait.js
+++ 08/asyncAwait.js
@@ -12,7 +12,7 @@ async function getData(){
 //if it returns a value then it will be automatically wrapped inside a promise and return a promise
 
 async function handlePromise(){
-  await p;
+ const val =  await p;
 }
 
 
