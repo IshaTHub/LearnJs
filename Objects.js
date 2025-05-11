3c20bc1d062b846ee2dbbd1809724979522abe49
IshaTHub <ishatiwari0508@gmail.com>
2025-05-11 20:24:47 +0530

    Object.assign & deep cloning

--- Objects.js
+++ Objects.js
@@ -1,16 +1,46 @@
-let menu = {
-	width: 200,
-	height: 300,
-	title: "My menu",
+let user = {
+	name: "John Doe",
+	age: 30,
+	email: "john.doe@example.com",
 };
 
-function multiplyNumeric(menu) {
-	for (let key in menu) {
-		if (typeof menu[key] === "number") {
-			menu[key] = menu[key] * 2;
-		}
-	}
-	return menu;
+let clone = {};
+
+for (let key in user) {
+	clone[key] = user[key];
 }
-let newMenu = multiplyNumeric(menu);
-console.log(newMenu);
+
+clone.name = "isha";
+// console.log(user);
+// console.log(clone);
+
+
+// Object.assign
+let user1 = {
+    canView: true,
+    canEdit: false,
+};
+
+Object.assign(user, user1);
+console.log(user)
+
+let clone1 = Object.assign({}, user);
+
+// structuredClone(object) : perfroms deep copy or clone
+
+let user2 = {
+    name: "John",
+    sizes: {
+      height: 182,
+      width: 50
+    }
+  };
+  
+  let clone3 = structuredClone(user2);
+  
+  alert( user2.sizes === clone3.sizes ); // false, different objects
+  
+  // user and clone are totally unrelated now
+  user2.sizes.width = 60;    // change a property from one place
+  alert(clone3.sizes.width); // 50, not related
+
