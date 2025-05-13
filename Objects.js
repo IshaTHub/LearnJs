d8c4f2024f1b03c13e59f626292beb4ca7df8d0f
IshaTHub <ishatiwari0508@gmail.com>
2025-05-13 23:21:47 +0530

    quotes

--- Objects.js
+++ Objects.js
@@ -1,46 +1,12 @@
-let user = {
-	name: "John Doe",
-	age: 30,
-	email: "john.doe@example.com",
-};
 
-let clone = {};
+let guestList = "Guests:\n * John\n * Pete\n * Mary";
 
-for (let key in user) {
-	clone[key] = user[key];
-}
+alert(guestList);
 
-clone.name = "isha";
-// console.log(user);
-// console.log(clone);
+let str1 = "Hello\nWorld"; // two lines using a "newline symbol"
 
+// two lines using a normal newline and backticks
+let str2 = `Hello
+World`;
 
-// Object.assign
-let user1 = {
-    canView: true,
-    canEdit: false,
-};
-
-Object.assign(user, user1);
-console.log(user)
-
-let clone1 = Object.assign({}, user);
-
-// structuredClone(object) : perfroms deep copy or clone
-
-let user2 = {
-    name: "John",
-    sizes: {
-      height: 182,
-      width: 50
-    }
-  };
-  
-  let clone3 = structuredClone(user2);
-  
-  alert( user2.sizes === clone3.sizes ); // false, different objects
-  
-  // user and clone are totally unrelated now
-  user2.sizes.width = 60;    // change a property from one place
-  alert(clone3.sizes.width); // 50, not related
-
+alert(str1 == str2); // true
