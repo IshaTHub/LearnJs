ca7efee1c1e019ec1f5773adb897efdbf521bfb4
IshaTHub <ishatiwari0508@gmail.com>
2025-05-17 01:18:43 +0530

    strings methods

--- Objects.js
+++ Objects.js
@@ -1,12 +1,7 @@
-
-let guestList = "Guests:\n * John\n * Pete\n * Mary";
-
-alert(guestList);
-
-let str1 = "Hello\nWorld"; // two lines using a "newline symbol"
-
-// two lines using a normal newline and backticks
-let str2 = `Hello
-World`;
-
-alert(str1 == str2); // true
+// There are 3 types of quotes. Backticks allow a string to span multiple lines and embed expressions ${…}.
+// We can use special characters, such as a line break \n.
+// To get a character, use: [] or at method.
+// To get a substring, use: slice or substring.
+// To lowercase/uppercase a string, use: toLowerCase/toUpperCase.
+// To look for a substring, use: indexOf, or includes/startsWith/endsWith for simple checks.
+// To compare strings according to the language, use: localeCompare, otherwise they are compared by character codes.
