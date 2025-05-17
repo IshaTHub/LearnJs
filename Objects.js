edd292fd9022319947b91f0c73e3eaca97514ae1
IshaTHub <ishatiwari0508@gmail.com>
2025-05-17 23:42:15 +0530

    truncate function using slice

--- Objects.js
+++ Objects.js
@@ -5,3 +5,21 @@
 // To lowercase/uppercase a string, use: toLowerCase/toUpperCase.
 // To look for a substring, use: indexOf, or includes/startsWith/endsWith for simple checks.
 // To compare strings according to the language, use: localeCompare, otherwise they are compared by character codes.
+
+// const ucFirst = (str) => {
+// 	return str[0].toUpperCase() + str.slice(1);
+// };
+
+// console.log(ucFirst("john")); // John
+
+function truncate(str, maxlength){
+    if(str.length <= maxlength){
+      return str;
+    }
+     if(str.length > maxlength){
+      let newStr = `${str.slice(0, maxlength -1)}...`;
+      return newStr;
+    }
+}	
+
+console.log(truncate("What I'd like to tell on this topic is:", 20)); // What I'd like to te...
