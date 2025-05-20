b50d3487afacede3c76c1d4b3c2fe974b0963b72
IshaTHub <ishatiwari0508@gmail.com>
2025-05-21 00:16:55 +0530

    forEach method

---
+++ Arrays.js
@@ -0,0 +1,46 @@
+let nums = [1,2,3,4,5]; 
+
+const forEachEx = (val) =>{
+console.log(val);
+}
+
+nums.forEach((val) => console.log(val));
+
+// console.log(nums)
+// console.log(String(nums) === '1,2,3,4,5')
+
+// console.log(arr1[0]);
+// arr1[2] = "Pear";
+
+// console.log(arr1[2])
+
+// console.log(arr1.length);
+
+// console.log(arr1);
+
+//from end
+// arr1.pop();
+
+// //console.log(arr1);
+// arr1.push('cherry');
+// //console.log(arr1)
+
+// //remove from beginning
+// arr1.shift()
+// //console.log(arr1);
+
+// //adds to beginning
+// arr1.unshift("Isha");
+//console.log(arr1)
+
+// for(key in arr1){
+//     console.log(arr1[key])
+// }
+
+// for(let fruit of fruits){
+//     console.log(fruit)
+// }
+
+
+
+
