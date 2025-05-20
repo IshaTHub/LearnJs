530cafaa9ffa2937e070d756df7e8f1587191033
IshaTHub <ishatiwari0508@gmail.com>
2025-05-21 00:43:36 +0530

    map(),split(),join()

--- Arrays.js
+++ Arrays.js
@@ -1,46 +1,15 @@
 let nums = [1,2,3,4,5]; 
 
-const forEachEx = (val) =>{
-console.log(val);
+function double(n){
+    return n*2;
 }
 
-nums.forEach((val) => console.log(val));
+let Arr1 = nums.map(double);
+//console.log(Arr1)
 
-// console.log(nums)
-// console.log(String(nums) === '1,2,3,4,5')
+let newArr = nums.filter((val) => val%2 === 0)
+//console.log(newArr)
 
-// console.log(arr1[0]);
-// arr1[2] = "Pear";
-
-// console.log(arr1[2])
-
-// console.log(arr1.length);
-
-// console.log(arr1);
-
-//from end
-// arr1.pop();
-
-// //console.log(arr1);
-// arr1.push('cherry');
-// //console.log(arr1)
-
-// //remove from beginning
-// arr1.shift()
-// //console.log(arr1);
-
-// //adds to beginning
-// arr1.unshift("Isha");
-//console.log(arr1)
-
-// for(key in arr1){
-//     console.log(arr1[key])
-// }
-
-// for(let fruit of fruits){
-//     console.log(fruit)
-// }
-
-
-
-
+let names = 'Bilbo, Gandalf, Nazgul';
+let arr = names.split(', ');
+console.log(arr);
