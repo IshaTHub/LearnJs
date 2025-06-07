b8d1c6da0f8da49271fe4eb60a331d56a0472b66
IshaTHub <ishatiwari0508@gmail.com>
2025-06-07 23:18:11 +0530

    map and sets

---
+++ MapSet.js
@@ -0,0 +1,34 @@
+let map = new Map();
+
+map.set('1', 'str1');   // a string key
+map.set(1, 'num1');     // a numeric key
+map.set(true, 'bool1'); // a boolean key
+
+// remember the regular Object? it would convert keys to string
+// Map keeps the type, so these two are different:
+alert( map.get(1)   ); // 'num1'
+alert( map.get('1') ); // 'str1'
+
+alert( map.size ); // 3
+
+
+let john = { name: "John" };
+
+// for every user, let's store their visits count
+let visitsCountMap = new Map();
+
+// john is the key for the map
+visitsCountMap.set(john, 123);
+
+alert( visitsCountMap.get(john) );
+
+let ram = { name: "John" };
+let ben = { name: "Ben" };
+
+let visitsCountObj = {}; // try to use an object
+
+visitsCountObj[ben] = 234; // try to use ben object as the key
+visitsCountObj[ram] = 123; // try to use john object as the key, ben object will get replaced
+
+// That's what got written!
+alert( visitsCountObj["[object Object]"] );
