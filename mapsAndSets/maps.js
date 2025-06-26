745c505d9b6c861ed16d3bc74ff48ede1e3d55d4
IshaT-Hub <ishatiwari0508@gmail.com>
2025-06-26 11:08:11 +0530

    map methods

---
+++ mapsAndSets/maps.js
@@ -0,0 +1,7 @@
+new Map()  // Creates a new Map object
+Map.set(Key, Value)  //stores valued by key
+Map.get(Key) // retrieves the value by key
+Map.has(Key) // true/false if key exists
+Map.delete(Key) // deletes the key and value
+Map.clear() // removes all keys and values
+Map.size // returns the current element count
