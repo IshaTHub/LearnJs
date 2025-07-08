aedff4cb5041b4ed9042ba67da14b1527a015135
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-07-08 23:56:59 +0530

    Update closures.js

--- closures.js
+++ closures.js
@@ -13,7 +13,7 @@ function x(){
 //console.log(z);
 //z();
 
-//this z will print function y(),
+this z will print function y(),
 // here y will remeber the lexical environment of x() where a = 1
 // so when we call z(), it will print 1
 
