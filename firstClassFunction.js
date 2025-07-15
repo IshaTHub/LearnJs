5be169af60188ae13a85aae9161c406fbcfcdfd1
IshaT-Hub <ishatiwari0508@gmail.com>
2025-07-15 11:54:20 +0530

    first class function

---
+++ firstClassFunction.js
@@ -0,0 +1,20 @@
+//function statement  aka function declaration
+function a(){
+    console.log("Hi");
+}
+
+//function expression
+var b = function(){
+    console.log("Hello");
+}
+
+
+//difference between function statement and function expression
+//function statement is hoisted, function expression is not
+
+//Anonymous function
+function (){
+    console.log("Anonymous function");
+}
+
+
